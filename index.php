d4a019f0fa4e74b9b781f0173a734defb07a33f2
applandtea <253421981@qq.com>
2017-07-20 09:51:55 +0800

    1

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+  $("p").click(function(){
+  $(this).hide();
+  });
+});
+</script>
+</head>
+
+<body>
+<p>123</p>
+</body>
+</html>
